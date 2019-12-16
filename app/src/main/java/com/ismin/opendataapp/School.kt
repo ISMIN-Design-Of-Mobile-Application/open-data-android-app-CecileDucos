929684d318230391a4270ba743d1e1771d59b5e6
Hossein <hossein.hashemi1206@gmail.com>
2019-12-16 23:33:39 +0100

    Update School.kt

--- app/src/main/java/com/ismin/opendataapp/School.kt
+++ app/src/main/java/com/ismin/opendataapp/School.kt
@@ -3,7 +3,11 @@ package com.ismin.opendataapp
 import java.io.Serializable
 
 
-data class School (val name : String, val ville : String,
-                   val typeEtablissement : String,
-                   val SiteInternet : String,  val region : String, val adresse : String,
-                   val latitude: Double, val  longitude : Double) : Serializable
+data class School (val name : String?,
+                   val ville : String?,
+                   val typeEtablissement : String?,
+                   val SiteInternet : String?,
+                   val region : String?,
+                   val adresse : String?,
+                   val latitude: Double,
+                   val  longitude : Double) : Serializable
